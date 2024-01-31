d2c50c60c63e051b6225c3bdb3cdef37b283aab0
CrisCL20 <cristian.soriano@usm.cl>
2024-01-31 17:22:17 -0300

    update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
+# Install stable version of neovim
 cd ~
 sudo apt-get install ninja-build gettext cmake unzip curl 
-git clone https://github.com/neovim/neovim
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+git clone https://github.com/neovim/neovim neovim_r
+cd neovim_r && make CMAKE_BUILD_TYPE=RelWithDebInfo
 git checkout stable
 sudo make install
+cd ~
+rm -rf neovim_r
 
+# Install nerdfonts
+cd ~
+git clone https://github.com/ryanoasis/nerd-fonts.git nerd-fonts
+cd nerd-fonts
+./install.sh 
+cd ~
+rm -rf nerd-fonts
+
+# Move config file into .config
 cd ~/.config 
-cp -r ~/nvim/nvim .
+mv -r ~/nvim/nvim .
 
